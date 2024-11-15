f38f762d841e703c23d7c6df9458839c233913d4
DeyviToyLokazo <144736582+DeyviToyLokazo@users.noreply.github.com>
2024-11-15 15:46:30 -0500

    Create AlumnoModel.java
    Creacion de Modelo ALumno

---
+++ MsSecurity_P/src/main/java/ExamenParcial/MsSecurity_P/model/AlumnoModel.java
@@ -0,0 +1,29 @@
+package ExamenParcial.MsSecurity_P.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="alumno")
+public class AlumnoModel {
+    @Id
+    @Column(name="idAlumno")
+    public Integer idAlumno;
+
+    @Column(name="nombre")
+    public String nombre;
+
+    @Column(name="nota")
+    public Double nota;
+
+}
