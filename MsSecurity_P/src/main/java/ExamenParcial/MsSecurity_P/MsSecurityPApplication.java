685e1a01c4b0169497e3305183ac9486432a2183
DeyviToyLokazo <144736582+DeyviToyLokazo@users.noreply.github.com>
2024-11-15 14:30:22 -0500

    Initial commit

---
+++ MsSecurity_P/src/main/java/ExamenParcial/MsSecurity_P/MsSecurityPApplication.java
@@ -0,0 +1,13 @@
+package ExamenParcial.MsSecurity_P;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSecurityPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSecurityPApplication.class, args);
+	}
+
+}
---
+++ MsSecurity_P/src/test/java/ExamenParcial/MsSecurity_P/MsSecurityPApplicationTests.java
@@ -0,0 +1,13 @@
+package ExamenParcial.MsSecurity_P;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSecurityPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
