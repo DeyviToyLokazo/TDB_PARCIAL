4f57aad86d3c2c0207d14fa5cc98da306c046a57
DeyviToyLokazo <144736582+DeyviToyLokazo@users.noreply.github.com>
2024-11-15 21:29:04 -0500

    Update AlumnoController.java

--- MsSecurity_P/src/main/java/ExamenParcial/MsSecurity_P/controller/AlumnoController.java
+++ MsSecurity_P/src/main/java/ExamenParcial/MsSecurity_P/controller/AlumnoController.java
@@ -3,8 +3,11 @@ package ExamenParcial.MsSecurity_P.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +32,20 @@ public class AlumnoController {
         return alumnoService.add(model);
     }
 
+    @GetMapping("/findById/{id}")
+    public AlumnoModel findById(@PathVariable Integer id) {
+        return alumnoService.findById(id);
+    }
 
+    @PutMapping("/update")
+    public AlumnoModel update(@RequestBody AlumnoModel model){
+    return alumnoService.update(model);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        boolean isDeleted = alumnoService.delete(id);
+        return isDeleted ? "Usuario eliminado correctamente" : "Error al eliminar usuario";
+    }
 
 }
